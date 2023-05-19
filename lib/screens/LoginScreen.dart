7910977797cb49f1672a6c77af32c9db4fdff2f1
Amanjot Singh <amanjot02121202021@msijanakpuri.com>
2023-05-19 22:53:59 +0530

    Updates to login screen

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -1,3 +1,4 @@
+import 'package:chatgpt_app/const.dart';
 import 'package:flutter/material.dart';
 import 'package:chatgpt_app/Utils/Routes.dart';
 
@@ -14,29 +15,136 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 90),
-                child: Image.asset(
-                  'images/aiblack.png',
-                  height: 100,
-                  width: 100,
-                  scale: 5,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 90),
+                  child: Image.asset(
+                    'images/aiblack.png',
+                    height: 100,
+                    width: 100,
+                    scale: 5,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 45,
-              ),
-              const Text(
-                "Welcome Back",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+                const SizedBox(
+                  height: 40,
                 ),
-              )
-            ],
+                const Text(
+                  "Welcome Back",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                //Email and password textformfields
+                Form(
+                  // key: _formkey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      //Email address textformfield
+                      Padding(
+                        padding: const EdgeInsets.only(right: 18, left: 18),
+                        child: TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            label: Text(
+                              "Email address",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 18,
+                              ),
+                            ),
+                            hintText: "jaycarter@gmail.com",
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.grey,
+                                width: 1,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.grey,
+                                width: 1,
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.grey,
+                                width: 1,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      //Password textformfield
+                      Padding(
+                        padding: const EdgeInsets.only(right: 18, left: 18),
+                        child: TextFormField(
+                          textInputAction: TextInputAction.done,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            label: Text(
+                              "Password",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 18,
+                              ),
+                            ),
+                            hintText: "*******",
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.grey,
+                                width: 1,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.grey,
+                                width: 1,
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.grey,
+                                width: 1,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: green,
+                    padding:
+                        EdgeInsets.symmetric(vertical: 20, horizontal: 162),
+                  ),
+                  onPressed: () {},
+                  child: Text("Continue"),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text("Don't have an account? ")
+              ],
+            ),
           ),
         ),
       ),
