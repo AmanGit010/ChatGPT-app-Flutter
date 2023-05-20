20a2a12a50b4a54386df8ba93e435e6cd911e34f
Amanjot Singh <amanjot02121202021@msijanakpuri.com>
2023-05-20 18:01:48 +0530

    Added code back to the repo

---
+++ lib/Utils/Routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static String home = "/home";
+  static String login = "/login";
+  static String chat = "/chat";
+  // static String login = "/login";
+}
---
+++ lib/const.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color scaffoldBackgroundColor = Color(0xFF343541);
+Color cardColor = Color(0xFF444654);
+Color green = Color(0XFF16A281);
