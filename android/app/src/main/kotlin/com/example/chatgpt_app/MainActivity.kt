4871262b9fd63f2331ca2a376b63c70ea2447bea
Amanjot Singh <amanjot02121202021@msijanakpuri.com>
2023-05-20 17:59:43 +0530

    Refactored repo

--- android/app/src/main/kotlin/com/example/chatgpt_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.chatgpt_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/Utils/Routes.dart
+++
@@ -1,6 +0,0 @@
-class Routes {
-  static String home = "/home";
-  static String login = "/login";
-  static String chat = "/chat";
-  // static String login = "/login";
-}
